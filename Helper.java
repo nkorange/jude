f4c8262b99d9e0064f87d4ae22a6fcdd7db65c94
nkorange <zpf.073@gmail.com>
2015-09-17 00:16:41 +0800

    update Helper

--- Helper.java
+++ Helper.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Helper methods and variables.
@@ -16,7 +18,12 @@ public class Helper {
     public static BufferedWriter pusher; // push some temporary codes to file
     public static BufferedReader poper; // pop the temporary codes back
 
+    public static BufferedWriter initCodePusher;
+    public static BufferedReader initCodePoper;
+
     public static boolean pushFlag = false;
+
+    public static List<String> codes = new LinkedList<String>();
 
     public static void turnOnPusher() {
         pushFlag = true;
@@ -64,7 +71,48 @@ public class Helper {
         } catch (Exception e) {
             System.out.println("" + e);
         }
+    }
 
+    public static void pushInitCode(String code) {
+        try {
+            initCodePusher.write(code);
+            initCodePusher.flush();
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+    }
+
+    public static void pushInitCodeLine(String code) {
+        try {
+            initCodePusher.write(code);
+            initCodePusher.newLine();
+            initCodePusher.flush();
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+    }
+
+    public static void popInitCode() {
+        String line = null;
+        try {
+            line = initCodePoper.readLine();
+            while (line != null) {
+                writer.write(line);
+                writer.newLine();
+                writer.flush();
+                line = initCodePoper.readLine();
+            }
+
+            // At the end of the init procedure:
+            writer.write("\tleave");
+            writer.newLine();
+            writer.flush();
+            writer.write("\tret");
+            writer.newLine();
+            writer.flush();
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
     }
 
     public static void write(String code) {
@@ -92,13 +140,25 @@ public class Helper {
             reader = new BufferedReader(new FileReader("program.asm"));
             pusher = new BufferedWriter(new FileWriter("tmp.asm"));
             poper = new BufferedReader(new FileReader("tmp.asm"));
+            initCodePusher = new BufferedWriter(new FileWriter("init.asm"));
+            initCodePoper = new BufferedReader(new FileReader("init.asm"));
+            initCodePusher.write("_init:");
+            initCodePusher.newLine();
+            initCodePusher.flush();
+            initCodePusher.write("\tpush rbp");
+            initCodePusher.newLine();
+            initCodePusher.flush();
+            initCodePusher.write("\tmov rbp, rsp");
+            initCodePusher.newLine();
+            initCodePusher.flush();
+            codes.add("section .data");
         } catch (Exception e) {
             System.out.println("" + e);
         }
     }
 
     public static void end() {
-
+        fetchCode();
         try {
             writer.close();
             reader.close();
@@ -106,6 +166,22 @@ public class Helper {
             poper.close();
         } catch (Exception e) {
 
+        }
+    }
+
+    public static void storeCode(String code) {
+        codes.add(code);
+    }
+
+    public static void fetchCode() {
+        try {
+            for (String code : codes) {
+                writer.write(code);
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (Exception e) {
+            System.out.println("" + e);
         }
     }
 
